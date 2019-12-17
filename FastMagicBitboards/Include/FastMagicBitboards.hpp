6aba18cf9a193d83bc361891ced383cc252430ee
tearth <tearthdev@gmail.com>
2019-12-17 14:48:10 +0100

    Add method to generate permutations.

--- FastMagicBitboards/Include/FastMagicBitboards.hpp
+++ FastMagicBitboards/Include/FastMagicBitboards.hpp
@@ -3,6 +3,7 @@
 
 #include "Typedefs.hpp"
 #include "MagicStructure.hpp"
+#include "BitOperations.hpp"
 
 class FastMagicBitboards
 {
@@ -14,9 +15,14 @@ public:
 	
 private:
 	void generateMasks();
-	U64 generateMaskForRook(int field);
-	U64 generateMaskForBishop(int field);
+	U64 generateRookMask(int field);
+	U64 generateBishopMask(int field);
 	U64 generateMaskForDirection(int field, int shift);
+	void generateAttacks();
+	U64 generatePermutation(int permutationIndex, int field, U64 mask);
+	U64 generateRookAttacks(int field, U64 occupancy);
+	U64 generateBishopAttacks(int field, U64 occupancy);
+	U64 generateAttacksForDirection(int field, int shift);
 
 	Bitboard _rookAttacks[64][512];
 	Bitboard _bishopAttacks[64][512];
--- FastMagicBitboards/Source/FastMagicBitboards.cpp
+++ FastMagicBitboards/Source/FastMagicBitboards.cpp
@@ -13,18 +13,21 @@ FastMagicBitboards::~FastMagicBitboards()
 void FastMagicBitboards::Generate()
 {
 	generateMasks();
+	generateAttacks();
 }
 
 void FastMagicBitboards::generateMasks()
 {
 	for (int i = 0; i < 64; i++)
 	{
-		_rookMagicStructures[i].Mask = generateMaskForRook(i);
-		_bishopMagicStructures[i].Mask = generateMaskForBishop(i);
+		_rookMagicStructures[i].Mask = generateRookMask(i);
+		_rookMagicStructures[i].Offset = BitOperations::Count(_rookMagicStructures[i].Mask);
+		_bishopMagicStructures[i].Mask = generateBishopMask(i);
+		_bishopMagicStructures[i].Offset = BitOperations::Count(_bishopMagicStructures[i].Mask);
 	}
 }
 
-U64 FastMagicBitboards::generateMaskForRook(int field)
+U64 FastMagicBitboards::generateRookMask(int field)
 {
 	const U64 firstFile = 0x101010101010101;
 	const U64 firstRank = 0xff;
@@ -39,7 +42,7 @@ U64 FastMagicBitboards::generateMaskForRook(int field)
 	return mask;
 }
 
-U64 FastMagicBitboards::generateMaskForBishop(int field)
+U64 FastMagicBitboards::generateBishopMask(int field)
 {
 	U64 mask = 0;
 	mask |= generateMaskForDirection(field + 7, 7);
@@ -62,4 +65,36 @@ U64 FastMagicBitboards::generateMaskForDirection(int field, int shift)
 	}
 
 	return mask;
+}
+
+void FastMagicBitboards::generateAttacks()
+{
+	for (int i = 0; i < 64; i++)
+	{
+		int permutationsCount = 1 << _rookMagicStructures[i].Offset;
+		for (int p = 0; p < permutationsCount; p++)
+		{
+			U64 rookPermutation = generatePermutation(p, i, _rookMagicStructures[i].Mask);
+		}
+	}
+}
+
+U64 FastMagicBitboards::generatePermutation(int permutationIndex, int field, U64 mask)
+{
+	U64 permutation = 0;
+	while (mask != 0)
+	{
+		U64 lsb = BitOperations::GetLSB(mask);
+		mask = BitOperations::PopLSB(mask);
+
+		if ((permutationIndex & 1) != 0)
+		{
+			int lsbIndex = BitOperations::BitScan(lsb);
+			permutation |= (U64)1 << lsbIndex;
+		}
+
+		permutationIndex >>= 1;
+	}
+
+	return permutation;
 }
