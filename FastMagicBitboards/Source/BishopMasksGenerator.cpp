f8e1bb8e4f160e7af70669ba5815af5c40d5b1f5
tearth <tearthdev@gmail.com>
2019-12-21 15:30:22 +0100

    Fix invalid masks for bishop.

--- FastMagicBitboards/Source/BishopMasksGenerator.cpp
+++ FastMagicBitboards/Source/BishopMasksGenerator.cpp
@@ -5,10 +5,11 @@ U64 BishopMasksGenerator::Generate(int field)
 	assert(field >= 0 && field < 64);
 
 	U64 mask = 0;
-	mask |= generateMaskForDirection(field + 7, 7);
-	mask |= generateMaskForDirection(field - 7, -7);
-	mask |= generateMaskForDirection(field + 9, 9);
-	mask |= generateMaskForDirection(field - 9, -9);
+	mask |= generateMaskForDirection(field, 7);
+	mask |= generateMaskForDirection(field, -7);
+	mask |= generateMaskForDirection(field, 9);
+	mask |= generateMaskForDirection(field, -9);
 
+	mask &= ~((U64)1 << field);
 	return mask;
 }
