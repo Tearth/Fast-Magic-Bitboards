8fb6afc74f2ef96b5b4380b56566cf539d871d7c
tearth <tearthdev@gmail.com>
2019-12-23 23:13:06 +0100

    Add comments.

--- FastMagicBitboards/Include/FastMagicBitboards.hpp
+++ FastMagicBitboards/Include/FastMagicBitboards.hpp
@@ -23,17 +23,91 @@ class FastMagicBitboards
 public:
 	FastMagicBitboards();
 
+	//! Generates magic numbers for rooks.
+	/*!
+		Generates magic numbers for rooks and stores them in the internal structure. They can be obtained by
+		calling GetMagicStructures method. It usually takes a few hundred milliseconds.
+	*/
 	void GenerateForRook();
+
+	//! Generates magic numbers for rooks at the specified field.
+	/*!
+		Generates magic numbers for rooks at the specified field and stores them in the internal structure. They can be obtained by
+		calling GetMagicStructures method. It usually takes a few hundred milliseconds.
+		\param field Field index for which the magic number will be calculated.
+		\return Magic number calculated for the specified field.
+	*/
 	U64 GenerateForRook(int field);
+
+	//! Generates magic numbers for rooks at the specified field and shift.
+	/*!
+		Generates magic numbers for rooks at the specified field, shift and stores them in the internal structure. They can be obtained by
+		calling GetMagicStructures method. It usually takes a few hundred milliseconds.
+		\param field Field index for which the magic number will be calculated.
+		\param shift Shift used to prepare attacks table. The smaller value will require a smaller table.
+		\return Magic number calculated for the specified field.
+	*/
 	U64 GenerateForRook(int field, int shift);
+
+	//! Generates magic numbers for bishops.
+	/*!
+		Generates magic numbers for bishops and stores them in the internal structure. They can be obtained by
+		calling GetMagicStructures method. It usually takes a few hundred milliseconds.
+	*/
 	void GenerateForBishop();
+
+	//! Generates magic numbers for bishops at the specified field.
+	/*!
+		Generates magic numbers for bishops at the specified field and stores them in the internal structure. They can be obtained by
+		calling GetMagicStructures method. It usually takes a few hundred milliseconds.
+		\param field Field index for which the magic number will be calculated.
+		\return Magic number calculated for the specified field.
+	*/
 	U64 GenerateForBishop(int field);
+
+	//! Generates magic numbers for bishops at the specified field and shift.
+	/*!
+		Generates magic numbers for bishops at the specified field, shift and stores them in the internal structure. They can be obtained by
+		calling GetMagicStructures method. It usually takes a few hundred milliseconds.
+		\param field Field index for which the magic number will be calculated.
+		\param shift Shift used to prepare attacks table. The smaller value will require a smaller table.
+		\return Magic number calculated for the specified field.
+	*/
 	U64 GenerateForBishop(int field, int shift);
 
+	//! Retrieves bitboard representing available rook moves for the specified field and occupancy.
+	/*!
+		Retrieves bitboard representing available rook moves for the specified field and occupancy. Remember that you have to
+		call GenerateForRook or SetMagicStructures methods first.
+		\param field Field index for which the rook attacks will be retrieved.
+		\param occupancy Bitboard containing occupancy for which the attacks will be retrieved.
+		\return Bitboard representing available rook moves.
+	*/
 	U64 GetRookAttacks(int field, U64 occupancy);
+
+	//! Retrieves bitboard representing available bishop moves for the specified field and occupancy.
+	/*!
+		Retrieves bitboard representing available bishop moves for the specified field and occupancy. Remember that you have to
+		call GenerateForBishop or SetMagicStructures methods first.
+		\param field Field index for which the rook attacks will be retrieved.
+		\param occupancy Bitboard containing occupancy for which the attacks will be retrieved.
+		\return Bitboard representing available bishop moves.
+	*/
 	U64 GetBishopAttacks(int field, U64 occupancy);
 
+	//! Retrieves generated magic numbers and shifts.
+	/*!
+		Retrieves generated magic keys and stores them into the array with 128 elements (first 64 for rooks and last 64 for bishops).
+		\return Array with generated magic numbers and shifts.
+	*/
 	std::array<PersistentMagicStructure, 128> GetMagicStructures();
+
+	//! Sets magic numbers and shifts.
+	/*!
+		Sets magic numbers and shifts, and then generates for them tables with attacks for rooks and bishops.
+		\param persistentMagicStructures Array with magic numbers and shifts for rooks (first 64 elements) and bishops (last 64 elements).
+		\return True if attack tables have been generated with success (there was no any collision), otherwise false.
+	*/
 	bool SetMagicStructures(std::array<PersistentMagicStructure, 128> persistentMagicStructures);
 private:
 	U64 calculateField(int field, int shift, MagicStructure &pieceMagicStructures, std::unique_ptr<AttacksGeneratorBase> &attacksGenerator, std::unique_ptr<MasksGeneratorBase> &masksGenerator);
