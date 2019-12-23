e230a3470cac77e4a8d63bc19e3982cd3798b66e
tearth <tearthdev@gmail.com>
2019-12-23 18:32:43 +0100

    Add tests.

--- TestApp/TestApp.cpp
+++ TestApp/TestApp.cpp
@@ -6,11 +6,13 @@
 using namespace std;
 using namespace std::chrono;
 
-volatile U64 trap;
+void testRook(int field, U64 occupaction, U64 expectedResult);
+void testBishop(int field, U64 occupaction, U64 expectedResult);
+
+FastMagicBitboards fastBitboards;
+
 int main()
 {
-	FastMagicBitboards fastBitboards;
-
 	while (1)
 	{
 		auto start = chrono::steady_clock::now();
@@ -18,18 +20,38 @@ int main()
 		fastBitboards.GenerateForBishop();
 		auto end = chrono::steady_clock::now();
 
-		cout << "GenTime: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms, ";
+		cout << "GenTime: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl;
 
-		auto start2 = chrono::steady_clock::now();
-		U64 sum = 0;
-		for (int i = 0; i < 1000000000; i++)
-		{
-			uint64_t testOccupancy = 0xc0f900050000f834 + i;
-			sum += fastBitboards.GetRookAttacks(0, testOccupancy);
-		}
-		auto end2 = chrono::steady_clock::now();
-		trap = sum;
+		testRook(0, 0xc19da1890c182089, 0x10101010e);
+		testRook(35, 0xc19da1890c182089, 0x808f708000000);
+		testRook(52, 0xc19da1890c182089, 0x10e8101010100000);
+		testRook(63, 0xc19da1890c182089, 0x4080000000000000);
 
-		cout << "GetTime: " << chrono::duration_cast<chrono::milliseconds>(end2 - start2).count() << " ms" << endl;
+		testBishop(0, 0xc19da1890c182089, 0x8040200);
+		testBishop(35, 0xc19da1890c182089, 0x4122140014204080);
+		testBishop(52, 0xc19da1890c182089, 0x2800280402010000);
+		testBishop(63, 0xc19da1890c182089, 0x40200000000000);
+	}
+}
+
+void testRook(int field, U64 occupaction, U64 expectedResult)
+{
+	U64 result = fastBitboards.GetRookAttacks(field, occupaction);
+	if (result != expectedResult)
+	{
+		cout << "Invalid result for rook: field=" << field << ", occupacion=" << occupaction << endl;
+		cout << "  - result=" << result << endl;
+		cout << "  - expected result=" << expectedResult << endl;
+	}
+}
+
+void testBishop(int field, U64 occupaction, U64 expectedResult)
+{
+	U64 result = fastBitboards.GetBishopAttacks(field, occupaction);
+	if (result != expectedResult)
+	{
+		cout << "Invalid result for bishop: field=" << field << ", occupacion=" << occupaction << endl;
+		cout << "  - result=" << result << endl;
+		cout << "  - expected result=" << expectedResult << endl;
 	}
 }
